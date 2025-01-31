44d8cdb7b30632dc5bedf2df994803ca6d0196f0
James Nyakush <jnyakush99@gmail.com>
2025-01-31 22:10:36 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  data() {
+    return {
+      name: 'James Nyakundi',
+      status: true
+    }
+  },
+};
+</script>
+
+<template>
+
+  <div>
+
+    <h1>{{ name }}</h1>
+
+    <p v-if="status">Use is Active</p>
+    
+  </div>
+
+</template>
